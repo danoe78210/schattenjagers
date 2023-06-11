67df9a912b5b4b061912d01e9f095abcec0e7353
danoe78210 <131521590+danoe78210@users.noreply.github.com>
2023-06-11 18:00:43 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'La Saga des Schattenjägers'
+copyright = '2023, Danoë'
+author = 'Danoë'
 
 release = '0.1'
-version = '0.1.0'
+version = '0.0.1'
 
 # -- General configuration
 
